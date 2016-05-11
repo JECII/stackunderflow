f86c7211377a9e0ad58db0e29faf1e6853e12a87
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-05-11 17:14:50 -0600

    dashboard/questions

--- app/components/dashboard/dashboard-component.js
+++ app/components/dashboard/dashboard-component.js
@@ -1,14 +1,20 @@
 app.controller('DashboardController', function($rootScope, $scope){
-	/**
-	 * To edit the member object you must go through $rootScope.member
-	 * $rootScope.member is a $firebaseObject from AngularFire 
-	 * To see the methods at your disposal look here
-	 * https://www.firebase.com/docs/web/libraries/angular/api.html#angularfire-firebaseobject
-	 * 
-	 * Don't forget to call $rootScope.member.$save() after making changes to the $rootScope.member object
-     * 
-     * A feature request would be to add an edit profile button to the view that can show and hide the edit form in your html
-	 * 
-	 * */
+	
+	$scope.updateMember= function(){
+		
+	 $rootScope.member.$save()
+	 console.log($rootScope.member)
+	}
+	// /
+	//  * To edit the member object you must go through $rootScope.member
+	//  * $rootScope.member is a $firebaseObject from AngularFire 
+	//  * To see the methods at your disposal look here
+	//  * https://www.firebase.com/docs/web/libraries/angular/api.html#angularfire-firebaseobject
+	//  * 
+	//  * Don't forget to call $rootScope.member.$save() after making changes to the $rootScope.member object
+    //  * 
+    //  * A feature request would be to add an edit profile button to the view that can show and hide the edit form in your html
+	//  * 
+	//  * */
 	
 });
