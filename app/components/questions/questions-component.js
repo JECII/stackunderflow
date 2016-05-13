5754c50a248735a8ba8ec60c0074e686d1a08c55
nathan <nathan@Nathans-MacBook-Pro.local>
2016-05-12 21:37:55 -0600

    Bootstrap

--- app/components/questions/questions-component.js
+++ app/components/questions/questions-component.js
@@ -47,22 +47,22 @@ app.controller('QuestionController', function ($rootScope, $scope, question, com
 		$scope.responses = {}
 	}
 	
-	$scope.vote = function (v) {
-		console.log("value", v)
+	// $scope.vote = function (v) {
+	// 	console.log("value", v)
 		
 		
-		$scope.question.voteCount = 0;
-		for (var key in $scope.question.votes) {
-			$scope.question.voteCount += $scope.question.votes[key];
+	// 	$scope.question.voteCount = 0;
+	// 	for (var key in $scope.question.votes) {
+	// 		$scope.question.voteCount += $scope.question.votes[key];
 			
-				$scope.question.voteCount += v;
-				Console.log($scope.question.voteCount)
+	// 			$scope.question.voteCount += v;
+	// 			Console.log($scope.question.voteCount)
 			
 			
 			
-		}
-		$scope.question.$save()
-	}
+	// 	}
+	// 	$scope.question.$save()
+	// }
 	/**
 	 * The question, comments, responses arguments being passed into the controller  ^^^^^^^
 	 * come from the question route resolve,
